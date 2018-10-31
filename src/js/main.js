33cf2d560e8776c1327dc93996fd3f409c15c0ac
Ryan Otto <rotto@drew.edu>
2018-10-31 10:57:09 -0400

    Implement smoothscroll
    * Implement cross-platform smooth scrolling
    
    * Put contents inside a wrapper and container for smoothscroll implementation
    
    * Hide original scrollbar so new smoothscroll bar displays correctly

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  resizeWrapper();
+});
+$(window).resize(function() {
+  resizeWrapper();
+});
+
+//Ensure smoothscroll stability by resizing wrapper to window size
+function resizeWrapper(){
+  var windowWidth = $(window).width();
+  var windowHeight = $(window).height();
+  $("#wrapper").css('width', windowWidth);
+  $("#wrapper").css('height', windowHeight);
+  $("#wrapper").smoothWheel();
+}

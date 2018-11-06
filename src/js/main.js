24758a2796340a02e8061ef06dc3b9bc8648747c
Jonathon Allaire Smith <jonathonasmith@gmail.com>
2018-11-05 23:12:46 -0500

    Several Style Updates
    Navbar affixed to top of screen
    
    Link syntax fading
    
    animated fade in for all content elements
    
    updated contact info in footer
    
    changed primary color

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,4 @@
-// $(document).ready(function() {
-//   resizeWrapper();
-// });
-// $(window).resize(function() {
-//   resizeWrapper();
-// });
-//
-// //Ensure smoothscroll stability by resizing wrapper to window size
-// function resizeWrapper(){
-//   var windowWidth = $(window).width();
-//   var windowHeight = $(window).height();
-//   $("#wrapper").css('width', windowWidth);
-//   $("#wrapper").css('height', windowHeight);
-//   $("#wrapper").smoothWheel();
-// }
+ $(document).ready(function() {
+   $('.jumbotron').fadeIn();
+   $('.card').fadeIn();
+ });
